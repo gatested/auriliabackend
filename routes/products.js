aa5cbffd35a88e0a2672938c8b4e32eb39201d7d
unknown <gaelangelmelendez@gmail.com>
2025-06-05 19:52:43 -0600

    usedbackend

--- routes/products.js
+++ routes/products.js
@@ -12,5 +12,26 @@ router.get('/products', async (req, res) => {
 
     res.status(200).json(data)  
 })
+router.get('/products/isprincipal', async (req, res) => {
+    const { data, error } = await supabase
+        .from('products')
+        .select('*')
+        .eq('isPrincipal', true)
+
+    if (error) return res.status(400).json({ error: error.message })
+
+    res.status(200).json(data)  
+})
+router.get('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const { data, error } = await supabase
+       .from('products')
+       .select('*')
+       .eq('id', id)
+
+    if (error) return res.status(400).json({ error: error.message })
+
+    res.status(200).json(data)
+})
 
 export default router
